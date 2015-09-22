cdd77d6d95da5b5ce78c599333804b4ab7b3ea11
mikepitre <michaelepitre@gmail.com>
2015-09-22 13:38:20 -0500

    made keyboard disappear when pressing return or touch outside the keyboard

--- ControllingTheKeyboard/ViewController.swift
+++ ControllingTheKeyboard/ViewController.swift
@@ -8,16 +8,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    
+    @IBOutlet weak var text: UITextField!
+    
+    @IBOutlet weak var label: UILabel!
+    
+    @IBAction func submitButton(sender: AnyObject) {
+        
+        label.text = text.text
+        
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.text.delegate = self
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        
+        self.view.endEditing(true)
+        
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        
+        textField.resignFirstResponder()
+        
+        return true
+        
     }
 
 
